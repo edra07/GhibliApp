2c65c94c37f4dd3c01caf964684077a966537529
Edin Ramírez <22ramirez.edin@gmail.com>
2020-10-30 21:20:52 -0600

    Initial Commit

---
+++ GhibliApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GhibliApp
+//
+//  Created by Edin Ramírez on 30/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GhibliApp/GhibliAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GhibliAppApp.swift
+//  GhibliApp
+//
+//  Created by Edin Ramírez on 30/10/20.
+//
+
+import SwiftUI
+
+@main
+struct GhibliAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
